dc2ca4f82a9b3f9d89de17d26b3c072ac78263c1
margouillat <margouillat_ez@proton.me>
2026-08-16 22:27:43 +0200

    Ajout react router

--- open-front/src/main.tsx
+++ open-front/src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
+import { BrowserRouter, Route, Routes } from "react-router"
 import "./index.css"
-import App from "./App.tsx"
+import App from "./pages/App"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import Passport from "./pages/Passport"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<App />} />
+            <Route path=":passportId" element={<Passport />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 )
